9b357ed77b838620ec38daa311b6a27ad0131dfb
Cuprum77 <blockland70@gmail.com>
2024-01-08 19:43:39 +0100

    updated the spi to follow university guidelines

--- SPI_Init/SPI.vhd
+++ SPI_Init/SPI.vhd
@@ -8,171 +8,153 @@ use ieee.std_logic_unsigned.all;
 -- This is a modified version of the SPI interface
 -- The SPI interface is modified to work with the display
 -- This is based off a clock of 100 MHz
---
-----------------------------------------------------------------------------
--- Pin Descriptions
-----------------------------------------------------------------------------
--- System Lines
---
--- SYS_CLK  | INPUT     | System Clock
--- SYS_RST  | INPUT     | System Reset
-----------------------------------------------------------------------------
--- SPI Lines
---
--- SDA      | OUTPUT    | Serial Data
--- SCL      | OUTPUT    | Serial Clock
--- CS       | OUTPUT    | Chip Select
--- DC       | OUTPUT    | Data/Command
--- RST      | OUTPUT    | Reset
-----------------------------------------------------------------------------
--- Display Lines
---
--- SEND     | INPUT     | Send Data
--- BIT_WIDTH| INPUT     | Bit Width
--- SET_DC   | INPUT     | Set Data/Command
--- DATA     | INPUT     | Data
--- DONE     | OUTPUT    | Done
-----------------------------------------------------------------------------
-
 
 entity SPIDriver is
-	port(
-        -- ports
-        sys_clk		: in std_logic;
-        sys_rst		: in std_logic;
-        -- modified spi interface
-        sda 		: out std_logic;
-        scl 		: out std_logic;
-        cs  		: out std_logic;
-        dc  		: out std_logic;
-        -- display DATA
-        send		: in std_logic;
-        bit_width 	: in std_logic_vector(2 downto 0);
-        set_dc		: in std_logic;
-        data 		: in std_logic_vector(31 downto 0);
-        done		: out std_logic
-    );
+  port(
+    -- ports
+    clk       : in std_logic; -- 100 MHz
+    rst       : in std_logic; -- Reset, active HIGH
+    -- modified spi interface
+    spi_sda   : out std_logic; -- SPI SDA (Data)
+    spi_scl   : out std_logic; -- SPI SCL (Clock)
+    spi_cs    : out std_logic; -- SPI CS (Chip Select)
+    spi_dc    : out std_logic; -- SPI DC (Data/Command)
+    send      : in std_logic;  -- Send, active HIGH
+    set_dc    : in std_logic;  -- If the SPI DC is set or not, active HIGH
+    done      : out std_logic; -- Done, when all the bits have been sent, active LOW
+    data      : in std_logic_vector(31 downto 0); -- Bits to be sent
+    bit_width : in std_logic_vector(2 downto 0)   -- Number of bits to send
+  );
 end entity SPIDriver;
 
 
 architecture RTL of SPIDriver is
-	type state_machine is (idle, start, shiftout, clk1, stop, hold); 
-	signal new_state : state_machine;
+  type state_machine is (idle, start, shiftout, clk1, stop, hold); 
+  signal spi_state : state_machine;
     
-	signal delay_cnt       	: std_logic_vector(1 downto 0);
-	signal delay_DONE_full 	: std_logic;
-	signal delay_DONE_half 	: std_logic;
-	signal delay_DONE      	: std_logic;
+  signal delay_cnt       	: std_logic_vector(1 downto 0);
+  signal delay_done_full 	: std_logic;
+  signal delay_done_half 	: std_logic;
+  signal delay_done      	: std_logic;
 
-	signal bit_cnt : integer range 0 to 31;
+  signal bit_cnt : integer range 0 to 31;
 	
 begin
 
-	delay_process : process(SYS_CLK)
-	begin
-	  if rising_edge(SYS_CLK) then
-			if SYS_RST = '1' or delay_DONE = '1' or new_state = idle or new_state = hold then
-				 delay_cnt <= (others => '0');
-			else
-				 delay_cnt <= delay_cnt + 1;
-			end if; 
-	  end if;
-	end process;
+  -- This process handles the delay counter
+  -- This is essentially a glorified frequency divider, which gets reset when the delay is done
+  delay_process : process(clk)
+  begin
+    if rising_edge(clk) then
+      if rst = '1' or delay_done = '1' or spi_state = idle or spi_state = hold then
+        delay_cnt <= (others => '0');
+      else
+        delay_cnt <= delay_cnt + 1;
+      end if; 
+    end if;
+  end process;
 
-	delay_DONE_full <= '1' when delay_cnt = "10" else '0';
-	delay_DONE_half <= '1' when delay_cnt = "01" else '0';
+  -- These are the delay signals, which gets squished into one
+  -- Where the full delay is 2 cycles and half delay is 1 cycle
+  -- Useful for setting the clock high and low with the appropriate delays
+  delay_done_full <= '1' when delay_cnt = "10" else '0';
+  delay_done_half <= '1' when delay_cnt = "01" else '0';
+  delay_done <= delay_done_half when spi_state = clk1 or spi_state = shiftout else delay_done_full;
 
-	delay_DONE <= delay_DONE_half when new_state = clk1 or new_state = shiftout else delay_DONE_full;
-
-	bit_cnt_process : process(SYS_CLK)
-	begin
-		if rising_edge(SYS_CLK)then
-			if SYS_RST = '1' or new_state = idle or new_state = hold then
-				case BIT_WIDTH is
-					when "000" => bit_cnt <= 7;
-					when "001" => bit_cnt <= 15;
-					when "010" => bit_cnt <= 17;
-					when "011" => bit_cnt <= 23;
+  -- This is the bit counter
+  -- It will set the number of bits to be shifted out and count down appropriately
+  bit_cnt_process : process(clk)
+  begin
+    if rising_edge(clk)then
+      if rst = '1' or spi_state = idle or spi_state = hold then
+        case bit_width is
+          when "000" => bit_cnt <= 7;
+          when "001" => bit_cnt <= 15;
+          when "010" => bit_cnt <= 17;
+          when "011" => bit_cnt <= 23;
                     when "100" => bit_cnt <= 31;
-					when others => bit_cnt <= 7;
-				end case;
-			elsif new_state = clk1 and delay_DONE = '1' and bit_cnt > 0 then
-				bit_cnt <= bit_cnt - 1;
-			end if;
-		end if;
-	end process;
+          when others => bit_cnt <= 7;
+        end case;
+      elsif spi_state = clk1 and delay_done = '1' and bit_cnt > 0 then
+        bit_cnt <= bit_cnt - 1;
+      end if;
+    end if;
+  end process;
 
-	-- set the DATA/command or d/c pin
-	DC <= SET_DC when SYS_RST = '0' else '0';
+  -- set the DATA/command or d/c pin
+  spi_dc <= set_dc when rst = '0' else '0';
 
-	interface_process : process(SYS_CLK)
-	begin
-	if rising_edge(SYS_CLK) then
-		if SYS_RST = '1' then
-			new_state <= idle;
-		else
-			case new_state is
-				-- waiting for a command
-				when idle =>
-					SDA <= '0';
-					SCL <= '0';
-					CS <= '1';
-					DONE <= '1';
-					
-					if SEND = '1' then
-						new_state <= start;
-					else
-						new_state <= idle;
-					end if;
-				-- start the transmission
-				when start =>
-					SCL <= '0';
-					CS <= '0';
-					DONE <= '0';
-					
-					if delay_DONE = '1' then
-						new_state <= shiftout;
-					end if;
-				-- shift out the DATA and set the clock low
-				when shiftout =>
-					SDA <= DATA(bit_cnt);
-					SCL <= '0';
-					
-					if delay_DONE = '1' then
-						new_state <= clk1;
-					end if;
-				-- set the clock high
-				when clk1 =>
-					SCL <= '1';
+  -- This is the main state machine
+  -- It should be able to shift out however many bits are needed
+  -- With the appropriate delays and clock signals
+  interface_process : process(clk)
+  begin
+  if rising_edge(clk) then
+    if rst = '1' then
+      spi_state <= idle;
+    else
+      case spi_state is
+        -- waiting for a command
+        when idle =>
+          spi_sda <= '0';
+          spi_scl <= '0';
+          spi_cs <= '1';
+          done <= '1';
+          
+          if SEND = '1' then
+            spi_state <= start;
+          else
+            spi_state <= idle;
+          end if;
+        -- start the transmission
+        when start =>
+          spi_scl <= '0';
+          spi_cs <= '0';
+          done <= '0';
+          
+          if delay_done = '1' then
+            spi_state <= shiftout;
+          end if;
+        -- shift out the DATA and set the clock low
+        when shiftout =>
+          spi_sda <= DATA(bit_cnt);
+          spi_scl <= '0';
+          
+          if delay_done = '1' then
+            spi_state <= clk1;
+          end if;
+        -- set the clock high
+        when clk1 =>
+          spi_scl <= '1';
 				
-					if delay_DONE = '1' then
-						if bit_cnt = 0 then
-							new_state <= stop;
-						else
-							new_state <= shiftout;
-						end if;
-					end if;
-				-- stop the transmission
-				when stop =>
-					SDA <= '0';
-					SCL <= '0';
-					
-					if delay_DONE = '1' then
-						new_state <= hold;
-					end if;
-				-- hold the transmission
-				when hold =>
-					CS <= '1'; 
-					DONE <= '1';
+          if delay_done = '1' then
+            if bit_cnt = 0 then
+              spi_state <= stop;
+            else
+              spi_state <= shiftout;
+            end if;
+          end if;
+        -- stop the transmission
+        when stop =>
+          spi_sda <= '0';
+          spi_scl <= '0';
+       
+        if delay_done = '1' then
+          spi_state <= hold;
+        end if;
+        -- hold the transmission
+        when hold =>
+          spi_cs <= '1'; 
+          done <= '1';
 
-					if SEND = '1' then
-						new_state <= start;
-					else
-						new_state <= idle;
-					end if;      
-			end case;
-		end if;
-	end if;
+          if SEND = '1' then
+            spi_state <= start;
+          else
+            spi_state <= idle;
+          end if;      
+      end case;
+    end if;
+  end if;
 end process;
 
 end RTL;
--- SPI_Init/Testbench/SPI.vht
+++ SPI_Init/Testbench/SPI.vht
@@ -5,88 +5,86 @@ entity SPI_TB is
 end SPI_TB;
 
 architecture SPI_ARCH of SPI_TB is
-    -- constants
-    constant clk_period : time := 10ns; -- 100 MHz
+  -- constants
+  constant clk_period : time := 10ns; -- 100 MHz
 
-    -- signals, these are all initialized to 0
-    signal sys_clk : std_logic := '0';
-    signal sys_rst : std_logic := '0';
-    signal sda : std_logic := '0';
-    signal scl : std_logic := '0';
-    signal cs : std_logic := '0';
-    -- signal DC : std_logic := '0'; -- we don't need this signal
-    signal send : std_logic := '0';
-    signal bit_width : std_logic_vector(2 downto 0) := (others => '0');
-    signal data : std_logic_vector(31 downto 0) := (others => '0');
-    signal done : std_logic := '0';
+  -- signals, these are all initialized to 0
+  signal clk        : std_logic := '0';
+  signal rst        : std_logic := '0';
+  signal spi_sda    : std_logic := '0';
+  signal spi_scl    : std_logic := '0';
+  signal spi_cs     : std_logic := '0';
+  signal send       : std_logic := '0';
+  signal done       : std_logic := '0';
+  signal bit_width  : std_logic_vector(2 downto 0) := (others => '0');
+  signal data       : std_logic_vector(31 downto 0) := (others => '0');
 
-    component SPIDriver is
-        port(
-            -- ports
-            sys_clk		: in std_logic;
-            sys_rst		: in std_logic;
-            -- modified spi interface
-            sda 		: out std_logic;
-            scl 		: out std_logic;
-            cs  		: out std_logic;
-            dc  		: out std_logic;
-            -- display DATA
-            send		: in std_logic;
-            bit_width 	: in std_logic_vector(2 downto 0);
-            set_dc		: in std_logic;
-            data 		: in std_logic_vector(31 downto 0);
-            done		: out std_logic
-        );
-    end component SPIDriver;
+  component SPIDriver is
+    port(
+      -- ports
+      clk       : in std_logic; -- 100 MHz
+      rst       : in std_logic; -- Reset, active HIGH
+      -- modified spi interface
+      spi_sda   : out std_logic; -- SPI SDA (Data)
+      spi_scl   : out std_logic; -- SPI SCL (Clock)
+      spi_cs    : out std_logic; -- SPI CS (Chip Select)
+      spi_dc    : out std_logic; -- SPI DC (Data/Command)
+      send      : in std_logic;  -- Send, active HIGH
+      set_dc    : in std_logic;  -- If the SPI DC is set or not, active HIGH
+      done      : out std_logic; -- Done, when all the bits have been sent, active LOW
+      data      : in std_logic_vector(31 downto 0); -- Bits to be sent
+      bit_width : in std_logic_vector(2 downto 0)   -- Number of bits to send
+    );
+  end component SPIDriver;
 
 begin
 
-    -- set up the component
-    UUT : SPIDriver
-        port map(
-            sys_clk => sys_clk,
-            sys_rst => sys_rst,
-            sda => sda,
-            scl => scl,
-            cs => cs,
-            dc => open,
-            send => send,
-            bit_width => bit_width,
-            set_dc => '0',
-            data => data,
-            done => done
-        );
+  -- set up the component
+  UUT : SPIDriver
+    port map(
+      clk       => clk,
+      rst       => rst,
+      spi_sda   => spi_sda,
+      spi_scl   => spi_scl,
+      spi_cs    => spi_cs,
+      spi_dc    => open,
+      send      => send,
+      set_dc    => '0',
+      done      => done,
+      data      => data,
+      bit_width => bit_width
+    );
 
-    -- init process, where code that only runs once goes
-    init_process : process
-    begin
-        sys_rst <= '1';
-        wait for 10 ns;
-        sys_rst <= '0';
-    wait;
-    end process init_process;
+  -- init process, where code that only runs once goes
+  init_process : process
+  begin
+    rst <= '1';
+    wait for 10 ns;
+    rst <= '0';
+  wait;
+  end process init_process;
 
-    -- clock process, generates the clock signal
-    clk_process : process
-    begin
-        sys_clk <= '0';
-        loop 
-            wait for clk_period/2;
-            sys_clk <= not sys_clk;
-        end loop;
-    wait;
-    end process clk_process;
+  -- clock process, generates the clock signal
+  clk_process : process
+  begin
+    clk <= '0';
+    loop 
+      wait for clk_period/2;
+      clk <= not clk;
+    end loop;
+  wait;
+  end process clk_process;
 
-    -- always process, where we generate our test signals
-    always_process : process
-    begin
-        wait for 15 ns; -- wait for the init process to finish
-        bit_width <= "100"; -- 8 bit
-        data <= x"aaaaaaaa"; -- 10101010
-        send <= '1';
-        wait for 5 ns; -- give the driver a clock cycle
-        send <= '0';
-    wait;
-    end process always_process;
+  -- always process, where we generate our test signals
+  always_process : process
+  begin
+    wait for 15 ns; -- wait for the init process to finish
+    bit_width <= "100"; -- 8 bit
+    data <= x"aaaaaaaa"; -- 10101010
+    send <= '1';
+    wait for 5 ns; -- give the driver a clock cycle
+    send <= '0';
+  wait;
+  end process always_process;
 
 end SPI_ARCH;
