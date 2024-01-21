02d2e949503f38d13e787e8e7e75a41bd2d35fc6
Cuprum77 <Blockland70@gmail.com>
2024-01-22 00:15:02 +0100

    modified top to accept hdmi

--- top_level/top.vhd
+++ top_level/top.vhd
@@ -1,20 +1,33 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+-- Xilinx specific libraries
+library UNISIM;
+use UNISIM.VComponents.all;
 
 -- Note, the 100 MHz PLL has been down-clocked to 50 MHz to cope with the PMOD output impedance!
 
 entity top is
   port (
     -- inputs
-    sysclk  : in std_logic; -- 125 MHz external clock
-    btn     : in std_logic;
+    sysclk        : in std_logic; -- 125 MHz external clock
+    btn           : in std_logic;
+    -- hdmi in
+    hdmi_rx_hpd   : out std_logic;
+    hdmi_rx_sda   : inout std_logic;
+    hdmi_rx_scl   : inout std_logic;
+    hdmi_rx_cec   : inout std_logic;
+    hdmi_rx_clk_n : in std_logic;
+    hdmi_rx_clk_p : in std_logic;
+    hdmi_rx_n     : in std_logic_vector(2 downto 0);
+    hdmi_rx_p     : in std_logic_vector(2 downto 0);
     -- outputs
-    jb      : out std_logic_vector (5 downto 0);
-    jc      : out std_logic_vector (5 downto 0);
-    jd      : out std_logic_vector (5 downto 0);
-    je      : out std_logic_vector (7 downto 0);
-    led     : out std_logic_vector (3 downto 0)
+    jb            : out std_logic_vector (5 downto 0);
+    jc            : out std_logic_vector (5 downto 0);
+    jd            : out std_logic_vector (5 downto 0);
+    je            : out std_logic_vector (7 downto 0);
+    led           : out std_logic_vector (3 downto 0)
   );
 end entity;
 
@@ -87,7 +100,7 @@ architecture RTL of top is
   end component;
 
   -- xilinx pll
-  component PLL_100M is
+  component pll_200 is
     port
     (-- Clock in ports
       -- Clock out ports
@@ -98,8 +111,30 @@ architecture RTL of top is
     );
   end component;
 
+  -- hdmi to rgb
+  component dvi2rgb_0 is
+    port (
+      tmds_clk_p    : in std_logic;
+      tmds_clk_n    : in std_logic;
+      tmds_data_p   : in std_logic_vector(2 downto 0);
+      tmds_data_n   : in std_logic_vector(2 downto 0);
+      refclk        : in std_logic;
+      arst          : in std_logic;
+      vid_pdata     : out std_logic_vector(23 downto 0);
+      vid_pvde      : out std_logic;
+      vid_phsync    : out std_logic;
+      vid_pvsync    : out std_logic;
+      pixelclk      : out std_logic;
+      apixelclklckd : out std_logic;
+      plocked       : out std_logic;
+      prst          : in std_logic
+    );
+  end component;
+
   signal clk        : std_logic := '0';
+  signal clk_200    : std_logic := '0';
   signal rst        : std_logic := '0';
+  signal clk_div    : std_logic_vector(2 downto 0) := (others => '0');
 
   -- spi bus
   signal sda        : std_logic := '0';
@@ -123,25 +158,22 @@ architecture RTL of top is
   signal x          : std_logic_vector(11 downto 0) := (others => '0');
   signal y          : std_logic_vector(11 downto 0) := (others => '0');
 
+  -- hdmi
+  signal sda_i      : std_logic;
+  signal sda_o      : std_logic;
+  signal sda_t      : std_logic;
+  signal scl_i      : std_logic;
+  signal scl_o      : std_logic;
+  signal scl_t      : std_logic;
+  signal hdmi_vde   : std_logic := '0';
+  signal hdmi_hsync : std_logic := '0';
+  signal hdmi_vsync : std_logic := '0';
+  signal hdmi_data  : std_logic_vector(23 downto 0) := (others => '0');
+
 begin
 
   -- map the reset button
   rst <= btn;
-
-  checkerboard_proc : process(clk)
-  begin
-    if rising_edge(clk) then
-      if x(4) = '0' and y(4) = '0' then
-        r <= (others => '0');
-        g <= (others => '0');
-        b <= (others => '0');
-      else
-        r <= (others => '1');
-        g <= (others => '1');
-        b <= (others => '1');
-      end if;
-    end if;
-  end process;
 
   -- map the PMOD connectors to the correct pins
   -- PMOD JB
@@ -169,17 +201,20 @@ begin
   jd(5) <= data(1);
 
   -- PMOD JE
-  je(0) <= vs;
+  --je(0) <= vs;
+  je(0) <= hdmi_vsync;
   je(1) <= pclk;
   je(2) <= scl;
   je(3) <= disp_rst_n;
-  je(4) <= hs;
-  je(5) <= de;
+  --je(4) <= hs;
+  je(4) <= hdmi_hsync;
+  --je(5) <= de;
+  je(5) <= hdmi_vde;
   je(6) <= cs;
   je(7) <= sda;
 
   -- map the sequencer module
-  sequencer_comp : sequencer
+  sequencer_inst : sequencer
     generic map (
       enable_resetter => true,
       invert_dc       => true
@@ -197,36 +232,36 @@ begin
     );
 
   -- map the rgb module
-  rgb_comp : rgb
-    generic map (
-      pixel_format  => "010",
-      h_area        => 480,
-      h_front_porch => 2,
-      h_sync        => 2,
-      h_back_porch  => 2,
-      v_area        => 960,
-      v_front_porch => 2,
-      v_sync        => 2,
-      v_back_porch  => 2
-    )
-    port map (
-      clk           => clk,
-      rst           => rst,
-      enable        => done,
-      r             => r,
-      g             => g,
-      b             => b,
-      x             => x,
-      y             => y,
-      rgb_pclk      => pclk,
-      rgb_de        => de,
-      rgb_vs        => vs,
-      rgb_hs        => hs,
-      rgb_data      => data 
-    );
+  -- rgb_inst : rgb
+  --   generic map (
+  --     pixel_format  => "010",
+  --     h_area        => 480,
+  --     h_front_porch => 2,
+  --     h_sync        => 2,
+  --     h_back_porch  => 2,
+  --     v_area        => 960,
+  --     v_front_porch => 2,
+  --     v_sync        => 2,
+  --     v_back_porch  => 2
+  --   )
+  --   port map (
+  --     clk           => clk,
+  --     rst           => rst,
+  --     enable        => done,
+  --     r             => r,
+  --     g             => g,
+  --     b             => b,
+  --     x             => x,
+  --     y             => y,
+  --     rgb_pclk      => pclk,
+  --     rgb_de        => de,
+  --     rgb_vs        => vs,
+  --     rgb_hs        => hs,
+  --     rgb_data      => data 
+  --   );
 
   -- map the heart module
-  heart_comp : heart
+  heart_inst : heart
     port map (
       clk => sysclk,
       rst => rst,
@@ -234,11 +269,51 @@ begin
     );
 
   -- map the pll module
-  pll_comp : PLL_100M
+  pll_inst : pll_200
     port map (
-      clk_out => clk,
+      clk_out => clk_200,
       reset   => '0',
       clk_in  => sysclk
     );
+
+  -- clk divider
+  clk_div_proc : process(clk_200)
+  begin
+    if rising_edge(clk_200) then
+      clk_div <= clk_div + 1;
+    end if;
+  end process;
+
+  -- effectively divide the 200 MHz clock by 4 to get 50 MHz
+  clk <= clk_div(2);
+
+  -- map the hdmi to rgb module
+  hdmi_inst : dvi2rgb_0
+    port map (
+      tmds_clk_p    => hdmi_rx_clk_p,
+      tmds_clk_n    => hdmi_rx_clk_n,
+      tmds_data_p   => hdmi_rx_p,
+      tmds_data_n   => hdmi_rx_n,
+      refclk        => clk_200,
+      arst          => not done,
+      vid_pdata     => data,
+      vid_pvde      => de,
+      vid_phsync    => hs,
+      vid_pvsync    => vs,
+      pixelclk      => open,
+      apixelclklckd => open,
+      plocked       => open,
+      prst          => '0'
+    );
+
+  hdmi_rx_hpd <= '1';
+
+  -- map the rgb inputs
+  --r <= x"00";
+  --g <= x"f4";
+  --b <= x"ff";
+  r <= hdmi_data(7 downto 0);
+  g <= hdmi_data(15 downto 8);
+  b <= hdmi_data(23 downto 16);
 
 end architecture;
