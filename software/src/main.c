db16f9f48a0ca27efb7bd40ba331c21397c27498
Cuprum77 <Blockland70@gmail.com>
2024-03-01 01:20:10 +0100

    moved tinyusb cbs to usb_device.c, changed to work on Pico for rapid prototyping

--- software/src/main.c
+++ software/src/main.c
@@ -4,18 +4,14 @@
 #include "pico/binary_info.h"
 #include "version.h"
 #include "touch.h"
-#include "tusb.h"
 #include "usb_device.h"
 
-#define LED_PIN 6
+#define LED_PIN 25
 
 #define TOUCH_I2C_SDA 	18
 #define TOUCH_I2C_SCL 	19
 #define TOUCH_I2C_RST 	20
 #define TOUCH_I2C_INT 	21
-
-// Define which LED to indicate the bootloader activity
-#define PICO_STDIO_USB_RESET_BOOTSEL_FIXED_ACTIVITY_LED LED_PIN
 
 // Declare the binary info
 bi_decl(bi_program_description("Touchscreen HID"));
@@ -56,6 +52,10 @@ int main()
     // Init the USB
     usb_device_init();
 
+    // Interval for transmitting touch data
+    uint32_t touch_interval = 100000;
+    uint32_t touch_timer = 0;
+    
     while (1)
     {
         // Blink the LED
@@ -64,76 +64,18 @@ int main()
         // Device task
         usb_device_task();
 
+        // Printout the touch data
+        if ((time_us_32() - touch_timer) >= touch_interval)
+        {
+            touch_timer = time_us_32();
+            printf("Status: %d, %d, %d, %d, %d\n", touch_status_data.buffer_status, touch_status_data.large_detect, touch_status_data.proximity_valid, touch_status_data.have_key, touch_status_data.num_touches);
+            for (size_t i = 0; i < 5; i++)
+            {
+                printf("Touch %d: %d, %d, %d\n", i, touch_points[i].x, touch_points[i].y, touch_points[i].size);
+            }
+        }
+        
         // Touch task
         usb_device_touch_data((touch_point_data_t*)touch_points, 5);
     }
-}
-
-//--------------------------------------------------------------------+
-// Device callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device is mounted
-void tud_mount_cb(void)
-{
-
-}
-
-// Invoked when device is unmounted
-void tud_umount_cb(void)
-{
-
-}
-
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
-    
-}
-
-// Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
-    
-}
-
-//--------------------------------------------------------------------+
-// USB HID
-//--------------------------------------------------------------------+
-
-static void send_hid_report(uint8_t report_id, uint32_t btn)
-{
-    
-}
-
-// Every 10ms, we will sent 1 report for each HID profile (keyboard, mouse etc ..)
-// tud_hid_report_complete_cb() is used to send the next report after previous one is complete
-void hid_task(void)
-{
-    
-}
-
-// Invoked when sent REPORT successfully to host
-// Application can use this to send the next report
-// Note: For composite reports, report[0] is report ID
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len)
-{
-    
-}
-
-// Invoked when received GET_REPORT control request
-// Application must fill buffer report's content and return its length.
-// Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
-{
-    return 0;
-}
-
-// Invoked when received SET_REPORT control request or
-// received data on OUT endpoint ( Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
-{
-    
 }
